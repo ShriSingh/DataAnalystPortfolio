f13871347123a8ababc3aa1c34e195d6ab9a82ff
ShriSingh <shrisingh9416@gmail.com>
2025-07-01 18:52:58 -0700

    Writing sql query for finding optimal skills with high demand and high salary

--- PostgreSQL Projects/project_sql/5_optimal_skills.sql
+++ PostgreSQL Projects/project_sql/5_optimal_skills.sql
@@ -8,3 +8,48 @@ for Data Analyst roles
 benefits(high salary), offering strategic insights for career development
 in data analysis
 */
+WITH skills_demand AS (
+    SELECT
+        skills.skill_id,
+        skills.skills,
+        COUNT(skills_to_jobs.job_id) AS demand_count
+    FROM job_postings_fact job_posts
+    INNER JOIN skills_job_dim skills_to_jobs
+            ON job_posts.job_id = skills_to_jobs.job_id
+    INNER JOIN skills_dim skills
+        ON skills_to_jobs.skill_id = skills.skill_id
+    WHERE
+        job_posts.job_title_short = 'Data Analyst' AND
+        job_posts.salary_year_avg IS NOT NULL AND
+        job_posts.job_work_from_home = TRUE
+    GROUP BY skills.skill_id
+), average_salary AS (
+    SELECT
+        skills.skill_id,
+        skills.skills,
+        ROUND(AVG(job_posts.salary_year_avg), 0) AS avg_salary
+    FROM job_postings_fact job_posts
+    INNER JOIN skills_job_dim skills_to_jobs
+        ON job_posts.job_id = skills_to_jobs.job_id
+    INNER JOIN skills_dim skills
+        ON skills_to_jobs.skill_id = skills.skill_id
+    WHERE
+        job_posts.job_title_short = 'Data Analyst' AND
+        job_posts.salary_year_avg IS NOT NULL AND
+        job_posts.job_work_from_home = TRUE
+    GROUP BY skills.skill_id
+)
+
+SELECT 
+    skills_demand.skill_id,
+    skills_demand.skills,
+    demand_count,
+    avg_salary
+FROM skills_demand
+    INNER JOIN average_salary
+        ON skills_demand.skill_id = average_salary.skill_id
+WHERE demand_count > 10
+ORDER BY 
+    avg_salary DESC,
+    demand_count DESC
+LIMIT 25;
