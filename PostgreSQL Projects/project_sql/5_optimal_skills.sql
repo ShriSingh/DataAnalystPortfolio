577d0e795a0ed43606e69a550a45f227e00586cb
ShriSingh <shrisingh9416@gmail.com>
2025-07-01 18:41:04 -0700

    Creating sql query for finding optimal skills with high demand and high salary

---
+++ PostgreSQL Projects/project_sql/5_optimal_skills.sql
@@ -0,0 +1,10 @@
+/*
+Question: What are the most optimal skills to learn (aka it's in 
+high demand and a high-paying skill)?
+- Identify skills in high demand and associated with high average salaries 
+for Data Analyst roles
+- Concentrates on remote position with specified salaries
+- Why? Targets skills that offer job security(high demand) and financial
+benefits(high salary), offering strategic insights for career development
+in data analysis
+*/
